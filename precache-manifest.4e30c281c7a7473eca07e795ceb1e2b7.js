35f5e707e9da69ad851b706f635a1829eb146d07
kennyozordi <kennyozordi@gmail.com>
2020-10-24 02:09:33 +0100

    Updates

---
+++ precache-manifest.4e30c281c7a7473eca07e795ceb1e2b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9e16a814e6ba0ef231ac9b21fe9d7e",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "e447763158ce6e45f6dc",
+    "url": "/react-to-do/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9d0021ba61d1c5e6e7c0",
+    "url": "/react-to-do/static/css/main.e4257439.chunk.css"
+  },
+  {
+    "revision": "e447763158ce6e45f6dc",
+    "url": "/react-to-do/static/js/2.f92d8019.chunk.js"
+  },
+  {
+    "revision": "dc18c2ffd44f5f763e9399d049013942",
+    "url": "/react-to-do/static/js/2.f92d8019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0021ba61d1c5e6e7c0",
+    "url": "/react-to-do/static/js/main.41bc9ab7.chunk.js"
+  },
+  {
+    "revision": "36f54460ca140b019f62",
+    "url": "/react-to-do/static/js/runtime-main.1a9166e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
